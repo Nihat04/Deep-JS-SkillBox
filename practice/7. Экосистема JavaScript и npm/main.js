4237eea1c62ab446a0eff19d8541a206fb57ad6e
Nihat04 <aliev.nihat4@gmail.com>
2024-04-22 15:51:24 +0500

    upload module 8

--- practice/7. Экосистема JavaScript и npm/main.js
+++ practice/7. Экосистема JavaScript и npm/main.js
@@ -5,10 +5,36 @@ let expireDateValid = false;
 let cvcValid = false;
 
 document.querySelector('#card-number').addEventListener('input', (e) => {
+    const payingSystemImg = document.querySelector('.paying-system');
     const validationResult = validateCardNumber(e.target.value)
     e.target.value = validationResult.value;
     cardNumberValid = validationResult.isValid;
     checkButtonAccess();
+
+    const payingSystemNUmber = validationResult.value[0];
+
+    switch(payingSystemNUmber) {
+        case '4':
+            //Visa
+            payingSystemImg.src = './src/img/visa.png'
+            break;
+        case '5':
+            //mastercard
+            payingSystemImg.src = './src/img/mastercard.png'
+            break;
+        case '3':
+            //American express
+            payingSystemImg.src = './src/img/american_express.png'
+            break;
+        case '2':
+            //МИР
+            payingSystemImg.src = './src/img/mir.png'
+            break;
+        default:
+            payingSystemImg.src = '#';
+            break;
+
+    }
 });
 
 document.querySelector('#expire-date').addEventListener('input', (e) => {
---
+++ practice/8. Сборка/src/main.js
@@ -0,0 +1,130 @@
+import 'babel-polyfill';
+
+import { validateCardNumber, validateExpireDate, correctExpireDate, validateCVC } from "./scripts/validator";
+import {el, setChildren} from 'redom';
+
+import visaLogo from './img/Visa.png';
+import mirLogo from './img/mir.png';
+import mastercardLogo from './img/mastercard.png';
+import americanExpressLogo from './img/American_Express.png';
+
+import './style.css'
+
+const main = el('main');
+
+const form = el('form', {
+    class: 'card-form'
+});
+
+const cardNumberInp = el('input', {
+    type: 'text',
+    class: 'card-input',
+    placeholder: '1234 5678 9012 3456',
+    required: true
+});
+
+const payingSystemImg = el('img', {
+    class: 'paying-system',
+    src: '#'
+})
+
+const expireDateInp = el('input', {
+    type: 'text',
+    class: 'card-input',
+    placeholder: '00/00',
+    required: true
+});
+
+const cvcInp = el('input', {
+    type: 'text',
+    class: 'card-input',
+    placeholder: '000',
+    required: true
+});
+
+const emailInp = el('input', {
+    type: 'email',
+    class: 'card-input',
+    placeholder: 'email@mail.ru',
+    required: true
+});
+
+const payBtn = el('button', 'Оплатить',{
+    disabled: true
+})
+
+setChildren(form, [
+    cardNumberInp, 
+    payingSystemImg, 
+    expireDateInp, 
+    cvcInp,
+    emailInp,
+    payBtn
+]);
+
+setChildren(main, form);
+setChildren(window.document.body, main);
+
+let cardNumberValid = false;
+let expireDateValid = false;
+let cvcValid = false;
+
+cardNumberInp.addEventListener('input', (e) => {
+    const validationResult = validateCardNumber(e.target.value)
+    e.target.value = validationResult.value;
+    cardNumberValid = validationResult.isValid;
+    checkButtonAccess();
+
+    const payingSystemNUmber = validationResult.value[0];
+
+    switch(payingSystemNUmber) {
+        case '4':
+            //Visa
+            payingSystemImg.src = visaLogo;
+            break;
+        case '5':
+            //mastercard
+            payingSystemImg.src = mastercardLogo;
+            break;
+        case '3':
+            //American express
+            payingSystemImg.src = americanExpressLogo;
+            break;
+        case '2':
+            //МИР
+            payingSystemImg.src = mirLogo;
+            break;
+        default:
+            payingSystemImg.src = '#';
+            break;
+
+    }
+});
+
+expireDateInp.addEventListener('input', (e) => {
+    const validationResult = validateExpireDate(e.target.value);
+    e.target.value = validationResult.value;
+    // expireDateValid = validationResult.isValid;
+    checkButtonAccess();
+});
+expireDateInp.addEventListener('blur', (e) => {
+    const validationResult = correctExpireDate(e.target.value);
+    e.target.value = validationResult.value;
+    expireDateValid = validationResult.isValid;
+    checkButtonAccess();
+});
+
+cvcInp.addEventListener('input', (e) => {
+    const validationResult = validateCVC(e.target.value);
+    e.target.value = validationResult.value;
+    cvcValid = validationResult.isValid;
+    checkButtonAccess();
+});
+
+function checkButtonAccess() {
+    if(cardNumberValid && expireDateValid && cvcValid) {
+        payBtn.disabled = false;
+    } else {
+        payBtn.disabled = true;
+    }
+}
